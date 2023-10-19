245e2076119c721d7331ddb85ee1d655e446b2ec
Alex Zungu <alexjidiga@gmail.com>
2023-10-19 05:48:25 -0700

    fixed else block

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-	else if (last < 6)
+	else
 	{
 		printf("Last digit of %d is %d and is less than 6\n", n, last);
 	}
