190985e306232b7ee256f44506cc837f367db7ad
Alex Zungu <alexjidiga@gmail.com>
2023-10-19 08:31:35 -0700

    loops

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0
+*/
+int main(void)
+{
+	int n = 48;
+	int m = 49;
+	int w = 50;
+	int c = 49;
+	int d = 50;
+	int b = 50;
+
+	while (n < 58)
+	{
+		while (m < 58)
+		{
+			while (w < 58)
+			{
+				putchar(n);
+				putchar(m);
+				putchar(w);
+				if ((n != 55))
+				{
+					putchar(44);
+					putchar(32);
+				}
+				w++;
+			}
+			m++;
+			d++;
+			w = d;
+		}
+		c++;
+		b++;
+		d = b;
+		m = c;
+		w = b;
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
