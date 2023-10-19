1ce968499ac0bbb745839ae2e84658a2df0b0a65
Alex Zungu <alexjidiga@gmail.com>
2023-10-19 05:20:53 -0700

    more string and array

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+include "main.h"
+/**
+*_strcat - concatenate two string
+*
+*@dest: str 1
+*
+*@src: str 2
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, j, k;
+
+	i = 0, j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+		j++;
+	for (k = 0; k <= j; k++, i++)
+		dest[i] = src[k];
+	return (dest);
+}
