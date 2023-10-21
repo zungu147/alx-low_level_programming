3ee24f5cb8a6f33611f39651876705aff3f0504d
Alex Zungu <alexjidiga@gmail.com>
2023-10-21 07:50:27 -0700

    declaration

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
 *_isupper - checks for uppercase
-*@c -correspond ASCII
+*@c: -correspond ASCII
 *
 *Return: 1 for uppercase or
 *0: for non
 */
 int _isupper(int c)
 {
-	int c = 65;
+	c = 65;
 
 	if (c < 91)
 	{
