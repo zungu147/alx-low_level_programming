7dbd286364dd0d866035488cabd9d1df6a621420
Alex Zungu <alexjidiga@gmail.com>
2023-10-21 08:19:00 -0700

    func for check digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*_isdigit - checks for digit
+*@c: arg
+*
+*Return: 1 for digit or
+*0: for non digit
+*/
+int _isdigit(int c)
+{
+	i = 48;
+	if (i < 48)
+	{
+		return (0);
+	}
+	else if (i < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
