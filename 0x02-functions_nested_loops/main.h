538c91cb9744a5c12531d890f109f566c384aeb7
Alex Zungu <alexjidiga@gmail.com>
2024-02-29 06:33:08 -0800

    correction of all tasks

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,9 @@
 void print_alphabet(void);
 int _putchar(char c);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+
