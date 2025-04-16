d1d40c55fcb443fb34ae77956d6ac3697e17d406
Alex Zungu <alexjidiga@gmail.com>
2025-04-16 15:26:30 -0700

    char comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: - Always 0
+ *
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	return (0);
+}
