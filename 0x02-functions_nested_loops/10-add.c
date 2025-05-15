ef6f77c1ac609c5577cd66a6dbd72d88969e4f82
Alex Zungu <alexjidiga@gmail.com>
2025-05-15 13:58:17 -0700

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two intergers
+ * @a: - first interger
+ * @b: - second interger
+ *
+ * Return: Total of intergers on success
+ */
+int add(int b, int a)
+{
+	return (a + b);
+}
